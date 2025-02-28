95fd27c01282e446a30d74754e93a7588af81663
Benjamin Nussbaum <bnussbau@users.noreply.github.com>
2025-02-28 18:47:02 +0100

    wip

--- resources/views/components/responsive-icons/wifi.blade.php
+++ resources/views/components/responsive-icons/wifi.blade.php
@@ -1,10 +1,10 @@
 @props(['strength', 'rssi'])
-<flux:tooltip content="Wi-Fi RSSI Level: {{ $strength }} db" position="bottom">
-    @if ($strength == 3)
+<flux:tooltip content="Wi-Fi RSSI Level: {{ $rssi }} db" position="bottom">
+    @if ($strength === 3)
         <flux:icon.wifi class="dark:text-zinc-200"/>
-    @elseif ($strength == 2)
+    @elseif ($strength === 2)
         <flux:icon.wifi-high class="dark:text-zinc-200"/>
-    @elseif ($strength == 1)
+    @elseif ($strength === 1)
         <flux:icon.wifi-low class="dark:text-zinc-200"/>
     @else
         <flux:icon.wifi-zero class="dark:text-zinc-200"/>
