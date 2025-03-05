35bf71fde28909bd3315b08f6453cd04ec3b9ff8
Benjamin Nussbaum <bnussbau@users.noreply.github.com>
2025-03-05 17:58:24 +0100

    wip

--- routes/console.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use App\Jobs\FetchProxyCloudResponses;
-
-Schedule::job(FetchProxyCloudResponses::class, [])->cron(sprintf('*/%s * * * *', intval(config('services.trmnl.proxy_refresh_minutes', 15))));
