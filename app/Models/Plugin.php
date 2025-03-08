4e158cdb55dbffb33f27217f3185cdd0c48c95a0
Benjamin Nussbaum <bnussbau@users.noreply.github.com>
2025-03-08 19:48:23 +0100

    feat: add logic for plugin management
    wip
    
    wip
    
    wip

--- app/Models/Plugin.php
+++ app/Models/Plugin.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Plugin extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                $model->uuid = Str::uuid();
+            }
+        });
+    }
 }
