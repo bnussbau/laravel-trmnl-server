57f66f2d2ba0a711d0203ed8f65e5256d3fd6e1d
Benjamin Nussbaum <bnussbau@users.noreply.github.com>
2025-06-01 22:19:16 +0200

    feat: create DeviceLog model on log request

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Jobs\GenerateScreenJob;
 use App\Models\Device;
+use App\Models\DeviceLog;
 use App\Models\User;
 use App\Services\ImageGenerationService;
 use Illuminate\Http\Request;
@@ -185,6 +186,10 @@ Route::post('/log', function (Request $request) {
     $logs = $request->json('log.logs_array', []);
     foreach ($logs as $log) {
         \Log::info('Device Log', $log);
+        DeviceLog::create([
+            'device_id' => $device->id,
+            'log_entry' => $log,
+        ]);
     }
 
     return response()->json([
